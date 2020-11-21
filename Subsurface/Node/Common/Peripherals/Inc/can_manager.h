ecd92a3d2b60ece575eaca8afb7821d52f995d23
Daniel Burke <danielseanburke@gmail.com>
2020-11-21 13:06:54 -0330

    Consolidate TxMsgSet methods into one template method

--- Subsurface/Node/Common/Peripherals/Inc/can_manager.h
+++ Subsurface/Node/Common/Peripherals/Inc/can_manager.h
@@ -7,30 +7,30 @@
 class CANManager
 {
 private:
-    CANInterface m_can_interface;
+    CANInterface* m_can_interface;
     uint8_t m_tx_buffer[8] = {0};
 
 public:
-    CANManager (CANInterface can_interface);
+    CANManager (CANInterface* can_interface);
 
-    bool transmit(void);
+    bool transmit();
 
     void setTxId(uint16_t can_id);
 
-    void txMsgSetBool(bool data, uint8_t position);
+    template<typename T>
+    bool txMsgSet(T data, uint8_t position = 0)
+    {
+        // Make sure we won't overflow
+        if (sizeof(T) + position > 8) { return false; }
 
-    void txMsgSetFloat(float data, uint8_t position);
-    void txMsgSetDouble(double data);
+        uint8_t* data_as_uint8_t = (uint8_t*)(&data);
+        for (uint8_t i = position; i < position + sizeof(T); i++)
+        {
+            m_tx_buffer[i] = data_as_uint8_t[i];
+        }
 
-    void txMsgSetUInt8(uint8_t data, uint8_t position);
-    void txMsgSetUInt16(uint16_t data, uint8_t position);
-    void txMsgSetUInt32(uint32_t data, uint8_t position);
-    void txMsgSetUInt64(uint64_t data);
-
-    void txMsgSetInt8(int8_t data, uint8_t position);
-    void txMsgSetInt16(int16_t data, uint8_t position);
-    void txMsgSetInt32(int32_t data, uint8_t position);
-    void txMsgSetInt64(int64_t data);
+        return true;
+    }
 };
 
 #endif
