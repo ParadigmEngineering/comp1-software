f5ee5b1c244a8a04f965ca6aae0b9e9cf3dea7e2
colton-smith <cnsmith@mun.ca>
2020-11-22 15:32:11 -0330

    Whitespace, add 2 empty lines between top level functions

--- Examples/socketio-python/client_logger.py
+++ Examples/socketio-python/client_logger.py
@@ -11,17 +11,21 @@ SOCKET_SERVER = 'http://127.0.0.1:5000'
 def connect():
     print("Connected to server")
 
+
 @sio.event
 def connect_error():
     print("Connection to server failed!")
+
 
 @sio.event
 def disconnect():
     print("Disconnected from server")
     
+    
 @sio.on("telemetry_log")
 def telemetry_log(telem):
     print(f"TELEM RECEIVED - {telem}")
+
 
 def main():
     log.info("Logging Thread Started")
@@ -33,6 +37,7 @@ def main():
             time.sleep(2)
         else:
             connected = True
+
 
 if __name__ == "__main__":
     try:
--- Examples/socketio-python/server.py
+++ Examples/socketio-python/server.py
@@ -7,22 +7,26 @@ sio = socketio.Server()
 # Wrap with a WSGI application - can also use 
 app = socketio.WSGIApp(sio)
 
-# Define event callbacks - special events
+# Define eve
+# nt callbacks - special events
 # sid - unique client id
 # environ - dictionary containing request info like HTTP header
 @sio.event
 def connect(sid, environ):
     print('connect', sid)
-
+ 
+ 
 @sio.event
 def disconenct(sid, environ):
     print('disconnect', sid)
+
 
 # User defined event handlers
 @sio.on("telemetry_log")
 def telemetry_log(sid, data):
     print("TELEMETRY FORWARDED TO LOGGER")
     sio.emit("telemetry_log", data)
+  
     
 if __name__=="__main__":
     # I dont understant this, our angular app will act as the server in production
