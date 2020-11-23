54beaef58e7337de16e324cef638ca3375ed3e6d
Akashf-mun <akashf@mun.ca>
2020-11-23 14:02:05 -0330

    Finish wrapping of remaining HAL_SPI functions

--- Subsurface/Node/Common/Peripherals/inc/SPIInterface.h
+++ Subsurface/Node/Common/Peripherals/inc/SPIInterface.h
@@ -28,5 +28,36 @@ public:
     HAL_StatusTypeDef TransmitReceive(uint8_t* dataOut, uint16_t size,
                                         uint32_t timeout);
 
+    // Master transmit and receive functions using interrupts
+    HAL_StatusTypeDef Transmit_IT(uint8_t* data, uint16_t size);
+    HAL_StatusTypeDef Receive_IT(uint16_t size);
+    HAL_StatusTypeDef TransmitReceive_IT(uint8_t* dataTransmit, uint16_t size);
+
+    // Master transmit and receive functions using DMA
+    HAL_StatusTypeDef Transmit_DMA(uint8_t* data, uint16_t size);
+    HAL_StatusTypeDef Receive_DMA(uint16_t size);
+    HAL_StatusTypeDef TransmitReceive_DMA(uint8_t* dataOut, uint16_t size);
+
+    // DMA control functions
+    HAL_StatusTypeDef DMAPause();
+    HAL_StatusTypeDef DMAResume();
+    HAL_StatusTypeDef DMAStop();
+
+    // Abort functions
+    HAL_StatusTypeDef Abort();
+    HAL_StatusTypeDef Abort_IT();
+
+    // Interrupt request handler function
+    void IRQHandler();
+
+    // Callback functions
+    void TxCpltCallback();
+    void RxCpltCallback();
+    void TxRxCpltCallback();
+    void TxHalfCpltCallback();
+    void RxHalfCpltCallback();
+    void TxRxHalfCpltCallback();
+    void ErrorCallback();
+    void AbortCpltCallback();
 };
 #endif
