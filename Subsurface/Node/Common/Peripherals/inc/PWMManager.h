c8eda895581e34ab9fd7d50929d16468ce545318
Ian Carroll <ianpatcarr@gmail.com>
2020-11-29 17:23:35 -0330

    Ported PWMInterface and PWMManager
    HAL Macros still need a work around, and the CMake was editted using Daniels new update with the GLOB. A timer and a channel were added to the PWMManager class.

---
+++ Subsurface/Node/Common/Peripherals/inc/PWMManager.h
@@ -0,0 +1,57 @@
+#include "PWMInterface.h"
+#include <cstdint>
+//#include "mock_PWMInterface.h"
+
+#ifndef PWMMANAGER_H
+#define PWMMANAGER_H
+
+class PWMManager
+{
+public:
+    
+    // Constructor
+    PWMManager(uint64_t clockSpeed, uint32_t preScaler, uint32_t period, 
+    uint32_t pulse, PWMInterface interface, TIM_HandleTypeDef timer, uint32_t channel);
+
+    // Start and Stop
+    void PWMStart();
+    void PWMStop();
+
+    // Getters
+    uint32_t getPeriodCycles();
+    uint32_t getPeriodMs();
+    uint32_t getPeriodUs();
+    uint8_t getDutyCyclePercent();
+    uint32_t getFrequencyHz();
+    uint32_t getPulseCycles();
+    uint32_t getPulseMs();
+    uint32_t getPulseUs();
+
+
+    // Setters
+    void setPeriodCycles(uint32_t newPeriod);
+    void setPeriodMs(uint32_t newPeriod);
+    void setPeriodUs(uint32_t newPeriod);
+    void setDutyCyclePercent(uint8_t newDutyCycle);
+    void setFrequencyHz(uint32_t newFrequency);
+    void setPulseCycles(uint32_t newPulse);
+    void setPulseMs(uint32_t newPulse);
+    void setPulseUs(uint32_t newPulse);
+
+    // Updates the member interface object which will apply any changes
+    // to member variables and call the PWMInterface methods
+    void updateInterface();
+
+private:
+
+    uint64_t m_clockSpeed;
+    uint32_t m_preScaler;
+    uint32_t m_period;
+    uint32_t m_pulse;                   
+    PWMInterface m_interface;
+    TIM_HandleTypeDef m_timer;
+    uint32_t m_channel;
+    //mock_PWMInterface m_mockInterface;
+
+};
+#endif
---
+++ Subsurface/Node/Common/Peripherals/src/PWMManager.cpp
@@ -0,0 +1,109 @@
+#include "PWMManager.h"
+
+PWMManager::PWMManager(uint64_t clockSpeed, uint32_t preScaler,
+ uint32_t period, uint32_t pulse, PWMInterface interface, TIM_HandleTypeDef timer, uint32_t channel)
+: m_clockSpeed(clockSpeed), m_preScaler(preScaler), m_period(period),
+m_pulse(pulse), m_interface(interface), m_timer(timer), m_channel(channel)
+{}
+
+void PWMManager::PWMStart()
+{
+    m_interface.setParameters(m_clockSpeed, m_preScaler, 
+    m_period, m_pulse, m_timer, m_channel);
+    
+    //m_mockInterface.setParameters(m_clockSpeed, m_preScaler, 
+    //m_period, m_pulse, m_timer, m_channel);
+    
+    m_interface.PWMStart();
+}
+
+void PWMManager::PWMStop()
+{
+    m_interface.PWMStop();
+}
+
+void PWMManager::setPulseCycles(uint32_t newPulse)
+{
+    m_pulse = newPulse;
+}
+
+uint32_t PWMManager::getPulseCycles()
+{
+    return m_pulse;
+}
+
+void PWMManager::setPulseMs(uint32_t newPulse)
+{
+    m_pulse = (newPulse / 1000.0) * ((float)m_clockSpeed / (float)m_pulse);
+}
+
+uint32_t PWMManager::getPulseMs()
+{
+    return m_pulse * 1000.0;
+}
+
+void PWMManager::setPulseUs(uint32_t newPulse)
+{
+    m_pulse = (newPulse / 1000000.0) * ((float)m_clockSpeed / (float)m_pulse);
+}
+
+uint32_t PWMManager::getPulseUs()
+{
+    return m_pulse * 1000000.0;
+}
+
+void PWMManager::setDutyCyclePercent(uint8_t newDutyCycle)
+{
+    m_pulse = m_period * (newDutyCycle / 100.0);
+}
+
+uint8_t PWMManager::getDutyCyclePercent()
+{
+    return ((float)m_pulse / (float)m_period) * 100.0;
+}
+
+uint32_t PWMManager::getPeriodCycles()
+{
+    return m_period;
+}
+
+void PWMManager::setPeriodCycles(uint32_t newPeriod)
+{
+    m_period = newPeriod;
+}
+
+void PWMManager::setPeriodMs(uint32_t newPeriod)
+{
+    m_period = (newPeriod / 1000.0) * ((float)m_clockSpeed / (float)m_preScaler);
+}
+
+uint32_t PWMManager::getPeriodMs()
+{
+    return m_period * 1000.0;
+}
+
+void PWMManager::setPeriodUs(uint32_t newPeriod)
+{
+    m_period = (newPeriod / 1000000.0) * ((float)m_clockSpeed / (float)m_preScaler);
+}
+
+uint32_t PWMManager::getPeriodUs()
+{
+    return m_period * 1000000.0;
+}
+
+void PWMManager::setFrequencyHz(uint32_t newFrequency)
+{
+    m_period = (1.0 / newFrequency) * ((float)m_clockSpeed / (float)m_preScaler);
+}
+
+uint32_t PWMManager::getFrequencyHz()
+{
+    return 1.0 / m_period;
+}
+
+void PWMManager::updateInterface()
+{
+    m_interface.setPeriod(m_period);
+    m_interface.setPulse(m_pulse);
+}
