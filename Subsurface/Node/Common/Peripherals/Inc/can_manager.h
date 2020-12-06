078b1347eded37c1414457fd4f6833053278ac53
Daniel Burke <danielseanburke@gmail.com>
2020-12-06 17:39:45 -0330

    Add unit testing, bug fixes
    Fixed the txMsgSet and getRxMsg methods based on results from unit
    testing

--- Subsurface/Node/Common/Peripherals/Inc/can_manager.h
+++ Subsurface/Node/Common/Peripherals/Inc/can_manager.h
@@ -1,8 +1,13 @@
 #ifndef NODE_CAN_MANAGER_H
 #define NODE_CAN_MANAGER_H
 
+#ifndef UNIT_TEST
 #include "can_interface.h"
+#else
+#include "mock_can_interface.h"
+#endif
 
+#include <cstring>
 
 class CANManager
 {
@@ -20,16 +25,33 @@ public:
 
     void setTxId(uint16_t can_id);
 
+    uint8_t* getTxBuffer() { return m_tx_buffer; }
+    uint8_t* getRxBuffer() { return m_rx_buffer; }
+
+#ifdef UNIT_TEST
+    void setRxBuffer(uint8_t* buffer, int len)
+    {
+        memset(m_rx_buffer, 0, 8);
+        for (int i = 0; i < len; ++i)
+        {
+            m_rx_buffer[i] = buffer[i];
+        }
+    }
+#endif // UNIT_TEST
+
     template<typename T>
     bool txMsgSet(T data, uint8_t position = 0)
     {
+        // Clear the txMsg Buffer
+        memset(m_tx_buffer, 0, 8);
         // Make sure we won't overflow
         if (sizeof(T) + position > 8) { return false; }
 
-        uint8_t* data_as_uint8_t = (uint8_t*)(&data);
-        for (uint8_t i = position; i < position + sizeof(T); i++)
+        uint8_t data_as_uint8_t[8] = { 0 };
+        memcpy(data_as_uint8_t, &data, sizeof(T));
+        for (uint8_t i = 0; i < sizeof(T); i++)
         {
-            m_tx_buffer[i] = data_as_uint8_t[i];
+            m_tx_buffer[i + position] = data_as_uint8_t[i];
         }
 
         return true;
@@ -39,9 +61,9 @@ public:
     T getRxMsg(uint8_t position = 0)
     {
         // If we would overflow, just start from 0
-        if (sizeof(T) + position > 8) { return (T)m_rx_buffer[0]; }
+        if (sizeof(T) + position > 8) { return *(T*)m_rx_buffer; }
 
-        return (T)m_rx_buffer[position];
+        return *(T*)(m_rx_buffer + position);
     }
 };
 
