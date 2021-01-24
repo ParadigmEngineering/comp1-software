d0fa11d04cf3c7988e93edf30b5a5d084f0001e7
Daniel Burke <danielseanburke@gmail.com>
2021-01-24 16:34:07 -0330

    Add blank lines between function declarations

--- Subsurface/Node/Common/Peripherals/Inc/can_interface.h
+++ Subsurface/Node/Common/Peripherals/Inc/can_interface.h
@@ -32,26 +32,34 @@ public:
 
     // Start CAN peripheral - returns true if successful
     bool start();
+
     // Stop CAN peripheral - returns true if successful
     bool stop();
 
     // Returns number of empty transmit mailboxes
     uint8_t getNumEmptyTxMailboxes();
+
     // Set transmit mailbox to CAN_TX_MAILBOX0,1,2
     void setTxMailbox(CanTxMailbox mailbox);
+
     // Check if a tx message is pending in CAN_TX_MAILBOX0,1,2
     bool isTxMessagePendingMailbox(CanTxMailbox mailbox);
+
     // Copy message to transmit mailbox - returns true if successful
     bool transmit(uint8_t *txData);
+
     // Set transmit CAN ID
     void setTxCanId(uint16_t can_id);
 
     // Get number of messages in recieve fifo 0
     uint8_t getNumMessagesFifo0();
+
     // Get number of messages in recieve fifo 1
     uint8_t getNumMessagesFifo1();
+
     // Set recieve fifo to CAN_RX_FIFO0,1
     void setRxFifo(CanRxFifo fifo);
+
     // Copy message from receive fifo - returns true if successful
     bool receive(uint8_t *rxData);
 };
