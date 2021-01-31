d19860f25638e0e38d4395fdd4f5b5ef3b81d9c8
colton-smith <cnsmith@mun.ca>
2021-01-31 16:36:40 -0330

    Use abs path to influx/grafana data folders

--- Services/influx-grafana/run_influx_grafana.sh
+++ Services/influx-grafana/run_influx_grafana.sh
@@ -2,12 +2,21 @@
 
 echo "--- Running influxdb and grafana docker container ---"
 
+cwd=$PWD
+influxPath="${cwd}/influx-data"
+grafanaPath="${cwd}/grafana-data"
+echo "Influx data path - $influxPath"  
+echo "Grafana data path - $grafanaPath"  
+
+echo "--- Executing docker run command ---"
+
+set -x
 docker run -d \
-  --name docker-influxdb-grafana \
-  -p 3003:3003 \
-  -p 3004:8083 \
-  -p 8086:8086 \
-  -v influx-data:/var/lib/influxdb \
-  -v grafana-data:/var/lib/grafana \
-  para-influx-grafana:1.0
-  
+    --name docker-influxdb-grafana \
+    -p 3003:3003 \
+    -p 3004:8083 \
+    -p 8086:8086 \
+    -v $influxPath:/var/lib/influxdb \
+    -v $grafanaPath:/var/lib/grafana \
+    para-influx-grafana:1.0 
+    
