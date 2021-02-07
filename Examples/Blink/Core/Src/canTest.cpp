1f12abb809ccdbdb0b4c4533090c5d5b891482da
Daniel Burke <danielseanburke@gmail.com>
2021-02-07 16:52:14 -0330

    Declare canTest in header and make main static

--- Examples/Blink/Core/Src/canTest.cpp
+++ Examples/Blink/Core/Src/canTest.cpp
@@ -1,29 +1,23 @@
-#include "Peripherals/Inc/can_manager.h"
-#include "can.h"
+#include "canTest.h"
 
-
-class CanTest
+void CanTest::main()
 {
-public:
-    void main()
+    MX_CAN_Init(); 
+    CANInterface interface(&hcan);
+    CANManager manager(&interface);
+
+    manager.setTxId(0x16);
+    uint32_t data = 62378;
+    manager.txMsgSet(data);
+
+    while (true)
     {
-        MX_CAN_Init(); 
-        CANInterface interface(&hcan);
-        CANManager manager(&interface);
+        manager.transmit();
 
-        manager.setTxId(0x16);
-        uint32_t data = 62378;
-        manager.txMsgSet(data);
-
-        while (true)
-        {
-            manager.transmit();
-
-            // Wait for a response message
-            while (!manager.receive()){};
-            auto u32Data = manager.getRxMsg<uint32_t>();
-            ++u32Data;
-            manager.txMsgSet(u32Data);
-        }
+        // Wait for a response message
+        while (!manager.receive()){};
+        auto u32Data = manager.getRxMsg<uint32_t>();
+        ++u32Data;
+        manager.txMsgSet(u32Data);
     }
-};
+}
