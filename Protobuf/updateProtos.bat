4a80daa29190f65b8f3fbb31bdf32b480ec347d0
Colton Smith <cnsmith@mun.ca>
2021-02-28 17:12:14 -0330

    Complete protobuf update batch script

--- Protobuf/updateProtos.bat
+++ Protobuf/updateProtos.bat
@@ -5,6 +5,7 @@ REM  This file compiles the .proto descriptor file for the proto packet that is 
 REM  It handles moving the created files to the appropriate folders for the flight computer.
 
 REM If proto compiler is installed 
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 WHERE protoc
 IF %ERRORLEVEL% NEQ 0 (
@@ -15,6 +16,32 @@ IF %ERRORLEVEL% NEQ 0 (
     echo Proto compiler found! Updating protos...
 )
 
+REM For any `*.proto` file, will create a folder, along with nested `cpp` & `py` folders, and compile `.proto` file
+for /R %%f in (*.proto) do (
+    set target_dir=%%~nf
+    MD "!target_dir!/cpp" "!target_dir!/py"
+    protoc --cpp_out="!target_dir!/cpp" "!target_dir!.proto"
+    protoc --python_out="!target_dir!/py" "!target_dir!.proto"
+)
+
+REM Move files accordingly
+SET flightComputer="..\Subsurface\FlightComputer\include"
+SET flightComputerSrc="..\Subsurface\FlightComputer\src"
+
+SET PDS="..\OnLand\PDS"
+SET FLASK="..\OnLand\ControlLaptop"
+
+REM Force copy the files, replacing existing ones
+REM /Y - Suppress confirm prompt
+REM /Y - Suppress confirm prompt
+
+COPY /Y Paradigm\cpp\*.pb.h %flightComputer%
+COPY /Y Paradigm\cpp\*.pb.cc %flightComputerSrc%
+
+COPY /Y Paradigm\py\* %PDS%
+COPY /Y Paradigm\py\* %FLASK%
+
+echo Proto Update Complete
                         
 
 
