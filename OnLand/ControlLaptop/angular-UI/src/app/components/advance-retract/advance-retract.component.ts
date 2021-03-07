2157c15f8cba5f57119873ee8e0360af48db43de
yeeteing <ytlo@mun.ca>
2021-03-07 14:38:57 -0330

    advance retract press down/up feature

--- OnLand/ControlLaptop/angular-UI/src/app/components/advance-retract/advance-retract.component.ts
+++ OnLand/ControlLaptop/angular-UI/src/app/components/advance-retract/advance-retract.component.ts
@@ -1,21 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-advance-retract',
-  templateUrl: './advance-retract.component.html',
-  styleUrls: ['./advance-retract.component.css']
+    selector: 'app-advance-retract',
+    templateUrl: './advance-retract.component.html',
+    styleUrls: ['./advance-retract.component.css']
 })
 export class AdvanceRetractComponent implements OnInit {
 
-  advanceFlag = false; 
+    advanceFlag = false;
+    timeoutHandler: NodeJS.Timeout;
+    count: number = 0;
 
-  toggleFlag(){
-    this.advanceFlag=!this.advanceFlag;
-  }
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngOnInit(): void {
+    }
+    toggleFlag() {
+        this.advanceFlag = !this.advanceFlag;
+    }
+
+    mouseDown() {
+        this.timeoutHandler = setInterval(() => {
+            this.count ++;
+            console.log(`press: ${this.count}`)
+        }, 500);
+    }
+
+    mouseUp() {
+        if (this.timeoutHandler) {
+            clearTimeout(this.timeoutHandler);
+            this.timeoutHandler = null;
+            this.count = 0;
+            console.log(`unpress: ${this.count}`)
+        }
+
+    }
 }
