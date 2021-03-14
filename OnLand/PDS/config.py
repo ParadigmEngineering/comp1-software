ba96ee6f605f2ff285738668174c776656683e90
colton-smith <cnsmith@mun.ca>
2021-03-14 16:20:14 -0230

    Add config.py file with some initial config values

---
+++ OnLand/PDS/config.py
@@ -0,0 +1,26 @@
+""" config.py
+
+Configuration values for:
+    - Influx
+    - Vehicle ECU TCP/UDP Connections
+    - Surface ECU TCP/UDP Connections
+"""
+
+# Influx
+INFLUX_HOST = "localhost"
+INFLUX_PORT = 8086
+INFLUX_NAME = "paradigm_boring"
+INFLUX_USER = "paradigm"
+INFLUX_PW = "boring"
+
+# TBC Packet
+TEAM_NAME = "Paradigm Boring"
+
+# SocketIO 
+SOCKET_SERVER = "http://localhost:5000"
+
+# Vehicle ECU
+VEHICLE_IP = ""
+
+# Surface ECU
+SURFACE_IP = ""
