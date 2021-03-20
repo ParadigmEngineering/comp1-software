df99928c75f20644a02cbb9e043dabc5c76f9793
Matthew Swenson <imprivatahackathon@gmail.com>
2021-03-20 19:54:45 -0400

    cleans up both server test scripts

--- Tools/boringNetworkTool/tcp_server.py
+++ Tools/boringNetworkTool/tcp_server.py
@@ -1,6 +1,23 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 8888))
+import sys
+
+HOST = ''
+PORT = 8888
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
+except socket.error as msg:
+    print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+
+
 s.listen(1)
 conn, addr = s.accept()
 while 1:
