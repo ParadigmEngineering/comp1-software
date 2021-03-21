e5b0130bfd74e3fefedb44d3f6d4a7d46dfa2850
yeeteing <ytlo@mun.ca>
2021-03-21 17:02:46 -0230

    allow dynamic change for alarms comp

--- OnLand/ControlLaptop/angular-UI/src/app/components/logo-bar/alarms/alarms.component.ts
+++ OnLand/ControlLaptop/angular-UI/src/app/components/logo-bar/alarms/alarms.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NaturalGasService } from 'src/app/services/natural-gas/natural-gas.service';
 
 @Component({
   selector: 'app-alarms',
@@ -7,17 +9,36 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./alarms.component.css']
 })
 export class AlarmsComponent implements OnInit {
-
-  armedFlag 
+  private telemetrySubscriber: Subscription;
+  armedFlag: string = "No Alarms";
+  armStyle = { 'background-color': '#3B824F' };
   private alarmsVar = ["No Alarms", "Communication Lost", "Gas Detected"];
 
-  toggleFlag(){
-    this.armedFlag=!this.armedFlag;
-  }
-  
-  constructor() { }
+  constructor(private naturalGas: NaturalGasService) { }
 
   ngOnInit(): void {
+    this.telemetrySubscriber = this.naturalGas.onNatural().subscribe((msg: { message: number }) => {
+      console.log('got a msg from server: ' + JSON.stringify(msg));
+      switch (parseInt(msg.message)) {
+        case 1:
+          this.armedFlag = this.alarmsVar[0];
+          this.armStyle = { 'background-color': '#3B824F' }
+          break;
+        case 2:
+          this.armedFlag = this.alarmsVar[1];
+          this.armStyle = { 'background-color': '#A10B0B' }
+          break;
+        case 3:
+          this.armedFlag = this.alarmsVar[2];
+          this.armStyle = { 'background-color': 'yellow' }
+          break;
+        default:
+      }
+    });
+
+  }
+  ngOnDestroy() {
+    this.telemetrySubscriber.unsubscribe();
   }
 
 }
