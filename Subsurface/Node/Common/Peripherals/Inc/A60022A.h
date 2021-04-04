d7bbd10b47067663e4abf226ec270f20d4355ac3
Ian Carroll <ianpatcarr@gmail.com>
2021-04-04 15:34:47 -0230

    Implemented A60022A sensor driver.

--- Subsurface/Node/Common/Peripherals/Inc/A60022A.h
+++ Subsurface/Node/Common/Peripherals/Inc/A60022A.h
@@ -1,15 +1,20 @@
 #ifndef A60022A_H
 #define A60022A_H
 
-// #include any drivers
+#include <cstdint>
+#include "ADCInterface.h"
+#include "EADCInterface.h"
 
 class A60022A
 {
-public:
-    A60022A::A60022A(/* args */);
-    double A60022A::GetOutput(/* args */); // output is 1uA/1°C
-private:
-    /* data */
+    public:
+        A60022A::A60022A(uint32_t resVal, ADC* ADC);
+        A60022A::A60022A(uint32_t resVal, EADC* EADC);
+        int A60022A::Read();
+    private:
+        uint32_t m_resVal;
+        ADC* m_ADC;
+        EADC* m_EADC;
 };
 
 #endif
--- Subsurface/Node/Common/Peripherals/Src/A60022A.cpp
+++ Subsurface/Node/Common/Peripherals/Src/A60022A.cpp
@@ -1,4 +1,40 @@
-#include "Common\Peripherals\Inc\A60022A.h"
+#include "Common/Peripherals/Inc/A60022A.h"
 
-A60022A::A60022A()
-    /* args {}*/
+A60022A::A60022A(uint32_t resVal, ADC* ADC) :
+    m_resVal(resVal),
+    m_ADC(ADC),
+    m_EADC(nullptr) {}
+
+A60022A::A60022A(uint32_t resVal, EADC* EADC) :
+    m_resVal(resVal),
+    m_ADC(nullptr),
+    m_EADC(EADC) {}
+
+int A60022A::Read()
+{
+    // using EADC Interface
+    if (m_ADC == nullptr)
+    {
+        int conversionFactor = 1; // 1uA / 1°C
+        int value = m_EADC->read(); // Voltage
+        int converted = m_resVal / value; // 1 / uA
+        if (value != 0 && m_resVal != 0)
+        {
+            int temp = 1 / (converted * conversionFactor); // °C!
+            return temp;
+        }
+    }
+
+    // using ADC Interface
+    else if (m_EADC == nullptr)
+    {
+        int conversionFactor = 1; // 1uA / 1°C
+        int value = m_ADC->read(); // Voltage
+        int converted = value / m_resVal; // 1 / uA
+        {
+            int temp = 1 / (converted * conversionFactor); // °C!
+            return temp;
+        }
+    }
+}
+   
