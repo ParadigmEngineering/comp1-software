5dcf45102dae8f23e9e41fe71496ada314e447c9
hhdtan <52300841+hhdtan@users.noreply.github.com>
2021-04-05 02:39:41 +0800

    displaylog skeleton
    Just initialising things.
    
    note: CSS not properly done because it is quite redundant at the moment. Standby until merge with other GUI task 3 components.

---
+++ OnLand/ControlLaptop/angular-UI/src/app/components/display-log/display-log.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-log',
+  templateUrl: './display-log.component.html',
+  styleUrls: ['./display-log.component.css']
+})
+export class DisplayLogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
