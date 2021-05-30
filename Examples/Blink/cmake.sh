fc922a4a7def61214dcdf36f92d6e4d7f80f1816
Ian Carroll <ianpatcarr@gmail.com>
2021-05-30 16:16:52 -0230

    Added newline, and command line parameter for building with Release or Debug configuration.

--- Examples/Blink/cmake.sh
+++ Examples/Blink/cmake.sh
@@ -3,5 +3,14 @@
 [ -d "build" ] && echo "Build folder already exists!" && cd build
 [ ! -d "build" ] && echo "Build folder does not exist. Creating now." &&  mkdir build && cd build
 
-cmake -DCMAKE_TOOLCHAIN_FILE=../arm-none-eabi-gcc.cmake -DCMAKE_BUILD_TYPE=Debug ..
+build_type = $1
+release = "Release"
+
+[if $build_type == $release]
+then
+    cmake -DCMAKE_TOOLCHAIN_FILE=../arm-none-eabi-gcc.cmake -DCMAKE_BUILD_TYPE=Release .. && echo "Building Release configuration."
+else
+    cmake -DCMAKE_TOOLCHAIN_FILE=../arm-none-eabi-gcc.cmake -DCMAKE_BUILD_TYPE=Debug .. && echo "Building Debug configuration."
+fi
+
 make
